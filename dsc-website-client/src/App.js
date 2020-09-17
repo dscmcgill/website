c4f0787f732f0d0a3d577854419e57fb138ff8ea
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2020-09-17 14:16:56 -0400

    React Router

--- dsc-website-client/src/App.js
+++ dsc-website-client/src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+// Pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Team from './pages/Team';
+import Events from './pages/Events';
+import PastEvents from './pages/PastEvents';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>App.js</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/team" component={Team} />
+          <Route exact path="/events" component={Events} />
+          <Route exact path="/pastevents" component={PastEvents} />
+        </Switch>
+      </Router>
     </div>
   );
 }
