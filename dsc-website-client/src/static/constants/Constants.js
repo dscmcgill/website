5b6986015e29232debefbb3452feb04055a31ff0
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2020-11-04 16:02:25 -0500

    Added constant file

---
+++ dsc-website-client/src/static/constants/Constants.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from '../images/dsc_logo.svg';
+
+import AdbIcon from '@material-ui/icons/Adb';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+
+export const timeline = [
+  {
+    time: '2019.12.01',
+    image: logo,
+    icon: <FastfoodIcon />,
+    title: 'title1',
+    subheader: 'subheader1',
+    content: `ey, altKey, metaKey, ge
+  rState, button, butjqnwdkjnsacn
+  qwdjer a collection of`
+  },
+  {
+    time: '2019.12.02',
+    image: logo,
+    icon: <AdbIcon />,
+    title: 'title2',
+    subheader: 'subheader2',
+    content: `dqwjdqlwldmlkqmskcmls
+  qwdijqoiejdqkckljneiojdfojqoeljcf
+  qwoifdjoiqejfclqnejnlcfjqwndlj`
+  },
+  {
+    time: '2019.12.03',
+    image: logo,
+    icon: <AdbIcon color="secondary" />,
+    title: 'title3',
+    subheader: 'subheader3',
+    content: `qoifhjcioqejfoicnjqew
+  qwudhuqehfchqeoijhfdpiqjwpfijcqew
+  qeijfdoiqejlcffiocijfe`
+  }
+];
