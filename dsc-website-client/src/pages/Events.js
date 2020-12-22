02b70893b89ac7ee83bd0fb0c82c86e1815a23fd
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2020-12-22 13:51:20 -0500

    pretty much done with the events

--- dsc-website-client/src/pages/Events.js
+++ dsc-website-client/src/pages/Events.js
@@ -5,6 +5,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CardMedia from '@material-ui/core/CardMedia';
 import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 // Events
 import css_events from '../static/images/upcomingEvents/css_event.jpeg';
@@ -17,6 +20,10 @@ const useStyles = makeStyles({
   },
   media: {
     width: '100%'
+  },
+  line: {
+    border: 'None',
+    borderRadius: '0px'
   }
 });
 
@@ -26,49 +33,104 @@ export default function Events() {
   return (
     <div id="events">
       <Grid
-        spacing={2}
+        spacing={1}
         container
         direction="row"
         justify="center"
-        alignItems="center"
-        style={{ height: '100vh' }}
+        alignItems="flex-start"
       >
-        <Grid container item xs={8}>
-          <Card className={classes.card}>
+        <Grid
+          container
+          item
+          xs={8}
+          style={{
+            padding: '100px 20px 100px 100px'
+          }}
+        >
+          <Card className={classes.line}>
             <CardMedia
               className={classes.media}
               src={tensor_flow_event}
               component="img"
               title="Tensorflow Event"
             />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Date
+              </Typography>
+              <Typography gutterBottom variant="h3" component="h2">
+                Tensor Flow workshop
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                learn tensorflow
+              </Typography>
+              <Button variant="contained">Sign up</Button>
+            </CardContent>
           </Card>
         </Grid>
-        <Grid container item xs={4}>
+        <Grid
+          container
+          item
+          xs={4}
+          style={{ padding: '100px', paddingLeft: '70px' }}
+        >
           <Grid
             container
             direction="column"
             justify="center"
             alignItems="center"
-            spacing={3}
+            spacing={5}
           >
             <Grid container item xs={12}>
-              <Card className={classes.card}>
+              <Card className={classes.line}>
                 <CardMedia
                   className={classes.media}
                   src={css_events}
                   component="img"
                   title="css event"
                 />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Date
+                  </Typography>
+                  <Typography gutterBottom variant="h3" component="h2">
+                    CSS
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Learn how to css
+                  </Typography>
+                  <Button variant="contained">Sign up</Button>
+                </CardContent>
               </Card>
             </Grid>
             <Grid container item xs={12} spacing={3}>
-              <Card className={classes.card}>
+              <Card className={classes.line}>
                 <CardMedia
                   className={classes.media}
                   src={react_events}
                   component="img"
                   title="react Event"
                 />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Date
+                  </Typography>
+                  <Typography gutterBottom variant="h3" component="h2">
+                    React
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Learn how to react
+                  </Typography>
+                  <Button variant="contained">Sign up</Button>
+                </CardContent>
               </Card>
             </Grid>
           </Grid>
